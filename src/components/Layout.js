9b592be58300619e76c39b7bfacf22ea8db5a145
BrOrlandi <brorlandi@gmail.com>
2017-04-07 23:10:03 -0300

    Fix eslints

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import boom from '../../assets/images/boom.gif';
+import PropTypes from 'prop-types';
 
 export default function Layout({ children }) {
   return (
@@ -10,4 +11,8 @@ export default function Layout({ children }) {
       <img src={boom} />
     </div>
   );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node 
 }
